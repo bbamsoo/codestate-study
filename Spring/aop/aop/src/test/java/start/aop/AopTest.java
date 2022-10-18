cd1d462adc89e997e52c9dad031b59ecd9bfcbc2
bbamsooexit <hiaro66@naver.com>
2022-10-18 16:33:55 +0900

    2022.10.18

---
+++ Spring/aop/aop/src/test/java/start/aop/AopTest.java
@@ -0,0 +1,42 @@
+package start.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import start.aop.order.aop.Aspect2;
+import start.aop.order.OrderRepository;
+import start.aop.order.OrderService;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@Slf4j
+@SpringBootTest
+@Import(Aspect2.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
